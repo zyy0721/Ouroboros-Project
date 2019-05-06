014af98635f7b5adba83e24f9508fb4bf738c750
zyy0721 <zhangyy_0721@163.com>
2019-05-06 15:34:00 +0800

    simple implementation of idea

--- handleIR.py
+++ handleIR.py
@@ -3,6 +3,13 @@ filename = 'test.ll'
 stackOp=[]
 stackLV=[]
 stackRV=[]
+class Statement:
+    def __init__(self):
+        self.leftVal = ''
+        self.Op = ''
+        self.firstType = ''
+        self.secondType = ''
+        self.rightVal = ''
 with open(filename,'r') as f:
     #skip useless information
     next(f)
@@ -16,30 +23,50 @@ with open(filename,'r') as f:
     next(f)
     for line in f:
         res2 = re.split(',| ',line)
-        print(res2)
-        #skip alloca 
-        if(res2[5] == 'alloca'):
-            continue
-        #skip return 0 case
-        if(res2[3] == 'store' and res2[5] == 0):
-            continue
-        
-        if(res2[5] == 'load'):
-            stackOp.append('load')
-            leftValue = res2[9]
-            leftValueType = res2[6]
-            stackLV.append(leftValue)
-        
-        if(res2[3] == 'store'):
-
-        #if res2[3] == 'store'
-
-        #if res2[5] == 'alloca' 
-
-        #if res2[5] == 'load'
-        print(line)
         if(res2[0]=='}\n'):
             break
+        #skip alloca
+        if( len(res2)>=5 and res2[4] == 'alloca'):
+            continue
+        #skip return 0 case
+        if(len(res2)>=3 and res2[2] == 'store' and res2[4] == 0):
+            continue
+        
+        if(len(res2)>=5 and res2[4] == 'load'):
+            stackOp.append('load')
+            tmpSta = Statement()
+            tmpSta.Op = 'load'
+            tmpSta.leftVal = res2[2]
+            tmpSta.firstType = res2[5]
+            tmpSta.secondType = res2[7]
+            tmpSta.rightVal = res2[8]
+            stackLV.append(tmpSta)
+        
+        if(len(res2)>=3 and res2[2] == 'store'):
+
+            tmpSta = Statement()
+            tmpSta.Op = 'store'
+            tmpSta.leftVal = res2[4]
+            tmpSta.firstType = res2[3]
+            tmpSta.rightVal = res2[7]
+            tmpSta.secondType = res2[6]
+            if(len(stackLV) == 1):
+                tmpStament = stackLV.pop()
+                if(tmpStament.leftVal == tmpSta.leftVal):
+                    print("Assign Statement: " +tmpSta.rightVal+ "=" + tmpStament.rightVal)
+
+            if(len(stackLV) == 2):
+                tmpStament2 = stackLV.pop()
+                tmpStament1 = stackLV.pop()
+                if(tmpStament1.leftVal == tmpSta.leftVal and tmpStament2.leftVal == tmpSta.rightVal):
+                    print("Deref Statement?: " + "*"+tmpStament2.rightVal + "=" + tmpStament1.rightVal)
+                if(tmpStament2.leftVal == tmpSta.leftVal and tmpStament1.leftVal == tmpStament2.rightVal):
+                    print("Ampersand Statement?: " + tmpSta.rightVal + "=" + "*" + tmpStament1.rightVal)
+
+
+
+
+
 
 
 print("Hello World!")
